adf6021d31a6978a42dd0bacc224c393e8b10b69
Alecu Radu Truica <radu.truica@gmail.com>
2018-07-17 10:27:11 -0700

    Initial Commit

---
+++ Meow Fest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meow Fest
+//
+//  Created by Alecu Truica on 7/17/18.
+//  Copyright © 2018 Alecu Truica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
